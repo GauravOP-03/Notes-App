4d42441ae907d6f21a2389d26cb74f96b1bf595f
Gaurav <gauravkumarop3@gmail.com>
2025-02-04 19:38:40 +0000

    adding package.json file

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const cors = require("cors");
 
 app.use(cors());
 
+const port = process.env.PORT || 5000;
 async function main() {
   await mongoose.connect(process.env.MONGO_URI);
 }
@@ -24,6 +25,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("app is listening");
 });
