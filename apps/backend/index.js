d7596a4dcea136448dc725e3f98163f3a1a7dd07
Gaurav <gauravkumarop3@gmail.com>
2025-06-25 10:11:08 +0530

    fixed docker error

--- apps/backend/index.js
+++ apps/backend/index.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
@@ -19,7 +19,7 @@ app.use(
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST"],
   },
@@ -194,6 +194,7 @@ io.on("connection", (socket) => {
   });
 });
 
+const port = process.env.PORT || 3000;
 server.listen(3000, () => {
   console.log("Server running on port 3000");
 });
